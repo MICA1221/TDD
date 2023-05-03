b149e4ac656fa8fb8d2e851408b18a3e53415af4
unknown <maricastrillon111@soy.sena.edu.co>
2023-05-03 10:49:23 -0500

    Proyecto creado

---
+++ CalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyMath;
+
+namespace MyMath.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Add_TwoPositiveNumbers_ReturnsCorrectSum()
+        {
+           
+            var calculator = new Calculator();
+            int a = 5;
+            int b = 3;
+            int expected = 8;
+
+            
+            int actual = calculator.Add(a, b);
+
+            
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
